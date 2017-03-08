4899c1278f3f7d5930b1cf8a9ed3b103a42f50c8
tapanakumarbadhai <tapanakumarbadhai@192.168.0.103>
2017-03-08 21:12:41 +0530

    some change

--- amzone/src/test/java/com/amzone/automationScript/Amzone_HomePage_LoginTest.java
+++ amzone/src/test/java/com/amzone/automationScript/Amzone_HomePage_LoginTest.java
@@ -1,5 +1,8 @@
 package com.amzone.automationScript;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
